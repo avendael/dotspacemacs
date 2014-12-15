10f90bb3a74ee468fe4657cbbfc726c2c1528caf
Evan Dale Aromin <evan@avendael.com>
2014-12-15 22:21:08 +0800

    Add `alchemist` to the list of packages to install

--- avendael/packages.el
+++ avendael/packages.el
@@ -1,6 +1,7 @@
 (defvar avendael-packages
   '(
     fasd
+    alchemist
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
