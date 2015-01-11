2409c510535afcbf74cadc1b354b8d715b587728
Evan Dale Aromin <evan@avendael.com>
2015-01-12 03:36:45 +0800

    Remove fasd in the private package layer

--- avendael/packages.el
+++ avendael/packages.el
@@ -1,7 +1,6 @@
 (defvar avendael-packages
   '(
     alchemist
-    fasd
     pretty-mode
     )
   "List of all packages to install and/or initialize. Built-in packages
@@ -14,15 +13,6 @@ which require an initialization must be listed explicitly in the list.")
   "List of packages to exclude.")
 
 ;; Package init
-(defun avendael/init-fasd ()
-  (use-package fasd
-    :init
-    (setq fasd-completing-read-function 'helm--completing-read-default)
-    :config
-    (progn
-      (global-fasd-mode 1)
-      (evil-leader/set-key "fa" 'fasd-find-file))))
-
 (defun avendael/init-pretty-mode ()
   (use-package pretty-mode
     :init
