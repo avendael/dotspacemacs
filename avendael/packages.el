496be80c8f6c54f0b9723e72803a1c3a4a203554
Evan Dale Aromin <evan@avendael.com>
2015-01-17 23:00:31 +0800

    Separate projectile and perspectives init

--- avendael/packages.el
+++ avendael/packages.el
@@ -6,7 +6,7 @@
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
-(defvar avendael-excluded-packages 
+(defvar avendael-excluded-packages
   '(ac-ispell
     neotree
     )
@@ -18,10 +18,14 @@ which require an initialization must be listed explicitly in the list.")
     :init
     (global-pretty-mode t)))
 
-(defun avendael/init-projectile ()
-  (use-package projectile
+(defun avendael/init-perspectives ()
+  (use-package perspective
     :init
     (progn
       (require 'persp-projectile)
-      (projectile-persp-bridge helm-projectile)
-      (setq projectile-switch-project-action 'helm-projectile))))
+      (projectile-persp-bridge helm-projectile))))
+
+(defun avendael/init-projectile ()
+  (use-package projectile
+    :init
+    (setq projectile-switch-project-action 'helm-projectile)))
