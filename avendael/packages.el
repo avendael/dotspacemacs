c4721cf7df5cd705e04370e6422eca70e31bbd64
Evan Dale Aromin <evan@avendael.com>
2015-12-26 14:54:00 +0800

    Add restclient and monokai to packages

--- avendael/packages.el
+++ avendael/packages.el
@@ -1,12 +1,12 @@
 (defvar avendael-packages
   '(
-    alchemist
     editorconfig
-    gh-md
     jsx-mode
     pretty-mode
     react-snippets
     yaml-mode
+    monokai-theme
+    restclient
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
